f7bacb960afe080089b7a2fdb6fca09909f4a292
Guilherme Alves Silveira <guilherme.alves.silve@gmail.com>
2023-04-26 01:01:22 -0300

    Update

--- vertx-database-stock-broker/src/main/java/br/com/guilhermealvesilve/broker/watchlist/PutWatchListFromDatabaseHandler.java
+++ vertx-database-stock-broker/src/main/java/br/com/guilhermealvesilve/broker/watchlist/PutWatchListFromDatabaseHandler.java
@@ -1,10 +1,14 @@
 package br.com.guilhermealvesilve.broker.watchlist;
 
 import br.com.guilhermealvesilve.broker.web.HttpResponses;
+import io.vertx.core.Future;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.pgclient.PgPool;
+import io.vertx.sqlclient.SqlConnection;
+import io.vertx.sqlclient.SqlResult;
 import io.vertx.sqlclient.templates.SqlTemplate;
 
+import java.util.List;
 import java.util.Map;
 
 public class PutWatchListFromDatabaseHandler extends AbstractWatchListFromDatabaseHandler {
@@ -24,11 +28,21 @@ public class PutWatchListFromDatabaseHandler extends AbstractWatchListFromDataba
         "asset", asset.name()))
       .toList();
 
-    SqlTemplate.forUpdate(pool,
-        "INSERT INTO broker.watchlist(account_id, asset) VALUES (#{account_id}, #{asset}) " +
-          "ON CONFLICT(account_id, asset) DO NOTHING")
-      .executeBatch(parameterBatch)
-      .onFailure(HttpResponses.internalServerErrorHandler("Failed to fetch watchlist for accountId: " + accountId, context))
-      .onSuccess(none -> HttpResponses.noContent(context));
+    pool.withTransaction(client ->
+      SqlTemplate.forUpdate(client,
+        "DELETE FROM broker.watchlist WHERE account_id = #{account_id}")
+        .execute(Map.of("account_id", accountId))
+        .onFailure(HttpResponses.internalServerErrorHandler("Failed to clear watchlist for accountId: " + accountId, context))
+        .compose(deletionDone -> addAllForAccountId(parameterBatch, client))
+        .onFailure(HttpResponses.internalServerErrorHandler("Failed to add watchlist for accountId: " + accountId, context))
+        .onSuccess(none -> HttpResponses.noContent(context)));
+  }
+
+  private Future<SqlResult<Void>> addAllForAccountId(final List<Map<String, Object>> parameterBatch,
+                                                     final SqlConnection client) {
+    return SqlTemplate.forUpdate(client,
+      "INSERT INTO broker.watchlist(account_id, asset) VALUES (#{account_id}, #{asset}) " +
+        "ON CONFLICT(account_id, asset) DO NOTHING")
+      .executeBatch(parameterBatch);
   }
 }
